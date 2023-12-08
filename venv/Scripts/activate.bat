2ad7e6b4c9b9307ab578bc2be5f54300496dcb1d
JovanSV64 <vuckovic.sv64.2021@uns.ac.rs>
2023-12-08 17:07:58 +0100

    removing venv

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\PROJECT_SOK\venv
+set VIRTUAL_ENV=C:\Users\jovan\Desktop\faks\sok\projekat\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
