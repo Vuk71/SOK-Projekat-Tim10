b0751bf259108988b00909dabbeb459861ea0dcf
JovanSV64 <vuckovic.sv64.2021@uns.ac.rs>
2023-12-08 17:29:32 +0100

    copied model from main branch

--- _platform/Core/services/model.py
+++ _platform/Core/services/model.py
@@ -1,1 +1,20 @@
 #node edges graph
+
+
+from typing import Dict, List
+
+class Graph:
+    def __init__(self):
+        self.nodes: Dict[str, Node] = {}
+        self.edges: List[Edge] = []
+
+class Node:
+    def __init__(self, id: str, data: Dict):
+        self.id = id
+        self.data = data
+
+class Edge:
+    def __init__(self, source: str, target: str, data: Dict = None):
+        self.source = source
+        self.target = target
+        self.data = data
