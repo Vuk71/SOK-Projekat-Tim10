e4c8f8034aebef9a63d1328bf9807670772097c4
JovanSV64 <vuckovic.sv64.2021@uns.ac.rs>
2024-02-03 22:55:23 +0100

    scripts for installing and uninstalling plugins

---
+++ install_plugins.py
@@ -0,0 +1,16 @@
+import subprocess
+
+def lay_egs(directory):
+    subprocess.run(["pip", "install", directory])
+
+def run_server(django_project):
+    subprocess.run(["python", f"{django_project}/manage.py", "makemigrations"])
+    subprocess.run(["python", f"{django_project}/manage.py", "migrate"])
+    subprocess.run(["python", f"{django_project}/manage.py", "runserver"])
+
+if __name__ == "__main__":
+    lay_egs("./_platform")
+    lay_egs("./api/plugins/data_source/github")
+    lay_egs("./api/plugins/visualizer/basic")
+    subprocess.run(["pip", "install", "setuptools"])
+    subprocess.run(["pip", "install", "django"])
