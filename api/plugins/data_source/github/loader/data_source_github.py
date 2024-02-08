a2346fd45e532d69c3209c4e8b975057521bec13
Katarina Krstin <92788177+kkatarinna@users.noreply.github.com>
2024-02-08 19:13:44 +0100

    data from github collected

--- api/plugins/data_source/github/loader/data_source_github.py
+++ api/plugins/data_source/github/loader/data_source_github.py
@@ -1,11 +1,20 @@
 from core.SOK.services.api import ParseDataBase
 from core.SOK.services.model import Graph,Node,Edge
+from github import Github
 
 class DataSourceGithub(ParseDataBase):
 
 
     def __init__(self: str):
+        self.github_token = "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"
+        self.repo = "BookingAppTeam18/IKS"
         self.account = ""
+
+    def set_token(self, token):
+        self.github_token = token
+
+    def set_repo(self, repository):
+        self.repo = repository
 
     def set_account(self, account):
         self.account = account
@@ -17,14 +26,44 @@ class DataSourceGithub(ParseDataBase):
         return "Load from github"
 
     def parse_data(self) -> Graph:
+        g = Github(self.github_token)
+        repo = g.get_repo(self.repo)
+        commits = repo.get_commits()
+        commits_list = [{"sha": commit.sha, "author": commit.author.login, "date": commit.commit.author.date, "message": commit.commit.message} for commit in commits]
+        print("loading data ...")
+        
+        # print(repo)
+        # for commit in commits_list:
+        #     print("autor: "+ commit["author"] +" message: "+ commit["message"])
+
+        print("making graph ...")
         graph = Graph()
-        node1 = Node(id="2",data = {'podatak':'neka vrednost'})
-        node2 = Node(id="3",data = {'podatak':'neka druga vrednost'})
-        edge = Edge(source = "2",target = "3")
-        graph.nodes[node1.id]=node1
-        graph.nodes[node2.id]=node2
-        graph.edges.append(edge)
+        # commit_nodes = {} 
+        # for commit in commits_list:
+        #     sha = commit["sha"]
+        #     author = commit["author"]
+        #     date = commit["date"]
+        #     message = commit["message"]
+
+        #     # Kreirajte cvor za svaki commit
+        #     node = Node(sha, {"author": author, "date": date, "message":message})
+        #     graph.nodes[sha] = node
+        #     commit_nodes[sha] = node
+
+        # # Dodajte grane na osnovu roditelja svakog commita
+        # for commit in commits:
+        #     sha = commit["sha"]
+        #     parents = commit_nodes[sha].parents
+
+        #     for parent_sha in parents:
+        #         edge = Edge(parent_sha, sha)
+        #         graph.edges.append(edge)
         return graph
 
     def __str__(self):
         return "load_github " + str(type(self))
+
+if __name__ == "__main__":
+    data_source = DataSourceGithub()
+    graph = data_source.parse_data()
+    print(graph)
