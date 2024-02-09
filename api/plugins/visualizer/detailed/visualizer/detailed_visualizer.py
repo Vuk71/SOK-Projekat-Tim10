2e6f88546238de428debbb9a28f4767f467fd318
JovanSV64 <vuckovic.sv64.2021@uns.ac.rs>
2024-02-09 19:28:11 +0100

    fixing edges sources and targets

--- api/plugins/visualizer/detailed/visualizer/detailed_visualizer.py
+++ api/plugins/visualizer/detailed/visualizer/detailed_visualizer.py
@@ -12,7 +12,7 @@ class DetailedVisualizer(VisualizeDataBase):
     def name(self):
         return "Detailed visualization graph"
 
-    def visualize_graph(self):
+    def visualize_graph(self, graph:Graph):
         # nodes = ""
         # edges = ""
         # for node in graph.nodes.values():
